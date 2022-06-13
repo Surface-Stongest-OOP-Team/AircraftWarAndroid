817b6b485d091f092395fa8547883feafbf4b333
shiqierha <2152640631@qq.com>
2022-06-13 21:20:27 +0800

    rank

--- app/src/main/java/edu/hitsz/application/RankListActivity.java
+++ app/src/main/java/edu/hitsz/application/RankListActivity.java
@@ -2,6 +2,7 @@ package edu.hitsz.application;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,9 +14,17 @@ public class RankListActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gradelist);
+        Button button2=(Button) findViewById(R.id.button2);
+        Button button3=(Button) findViewById(R.id.button3);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
 
+        }
     }
+
+
 }
